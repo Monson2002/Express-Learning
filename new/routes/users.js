7c9149d7e5b64881e6c53f308de206a5b5eedb14
monson2002 <monsonreji2002@gmail.com>
2023-11-11 11:34:19 +0530

    Add mongoose and routes to server (add/remove)

--- new/routes/users.js
+++ new/routes/users.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+// mongodb+srv://monson2002:<password>@cluster1-demo.swevm20.mongodb.net/?retryWrites=true&w=majority
+// HO5pde2iBylfC1zG
+const express = require('express');
+const router = express();
+const mongoose = require('mongoose');
+const User = require('./userModel');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const url = 'mongodb+srv://monson2002:HO5pde2iBylfC1zG@cluster1-demo.swevm20.mongodb.net/?retryWrites=true&w=majority';
 
-module.exports = router;
+mongoose.connect(url);
+
+router.get('/', (req, res) => {
+    res.send({"message":"Server is running"});
+})
+
+router.get('/addUser', async (req, res) => {
+    const newUser = await User.create({
+        id: 1,
+        name: "Monson",
+        email: "monsonreji2002@gmail.com",
+        age: 21,
+        number: "1234567890"
+    })
+    res.send(newUser);
+})
+
+router.get('/removeUser', async (req, res) => {
+    const newUser = await User.deleteOne({
+        name: "Monson",
+    })
+    res.send(newUser);
+})
+
+router.listen(3000);
